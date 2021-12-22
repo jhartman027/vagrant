0552de4879d686c3cd41c9e8c9fdbde83bc12a76
jhartman027 <jhartman027@msn.com>
2021-12-22 05:39:26 -0500

    Add files via upload

--- vagrant_util_restart.sh
+++ vagrant_util_restart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# Stop the vagrant vmware utility.
 sudo launchctl stop com.vagrant.vagrant-vmware-utility
 
 sleep 5
 
+# Start the vagrant vmware utility.
 sudo launchctl start com.vagrant.vagrant-vmware-utility
